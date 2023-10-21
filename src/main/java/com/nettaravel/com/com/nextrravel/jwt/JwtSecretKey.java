ce702edcf7dce49e4694bfb685f5c3a0147486fe
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-10-21 10:08:41 +0530

    create property jwtConfig

--- src/main/java/com/nettaravel/com/com/nextrravel/jwt/JwtSecretKey.java
+++ src/main/java/com/nettaravel/com/com/nextrravel/jwt/JwtSecretKey.java
@@ -1,10 +1,14 @@
 package com.nettaravel.com.com.nextrravel.jwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JwtSecretKey {
+    private final  JwtConfig jwtConfig;
 
-
-
+    @Autowired
+    public JwtSecretKey(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
 }
