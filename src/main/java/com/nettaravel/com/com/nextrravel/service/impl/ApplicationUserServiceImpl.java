f4f9ff342161faade139e0f771cff48c47ebde31
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-10-21 13:13:59 +0530

    create class  ApplicationUserServiceImpl implementation UserDetailsService

--- src/main/java/com/nettaravel/com/com/nextrravel/service/impl/ApplicationUserServiceImpl.java
+++ src/main/java/com/nettaravel/com/com/nextrravel/service/impl/ApplicationUserServiceImpl.java
@@ -2,8 +2,11 @@ package com.nettaravel.com.com.nextrravel.service.impl;
 
 import com.nettaravel.com.com.nextrravel.repo.UserRepo;
 import com.nettaravel.com.com.nextrravel.repo.UserRoleHasUserRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class ApplicationUserServiceImpl {
+public class ApplicationUserServiceImpl implements UserDetailsService {
 
     private final UserRepo  userRepo;
 
@@ -13,5 +16,10 @@ public class ApplicationUserServiceImpl {
     public ApplicationUserServiceImpl(UserRepo userRepo, UserRoleHasUserRepo userRoleHasUserRepo) {
         this.userRepo = userRepo;
         this.userRoleHasUserRepo = userRoleHasUserRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
