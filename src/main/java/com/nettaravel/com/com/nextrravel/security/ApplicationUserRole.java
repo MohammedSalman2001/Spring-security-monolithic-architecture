0666ec311ba7c1f527139d63457c20cf53350b75
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-10-21 09:49:15 +0530

    SimpleGrantedAuthority implementation

--- src/main/java/com/nettaravel/com/com/nextrravel/security/ApplicationUserRole.java
+++ src/main/java/com/nettaravel/com/com/nextrravel/security/ApplicationUserRole.java
@@ -1,4 +1,33 @@
 package com.nettaravel.com.com.nextrravel.security;
 
-public class ApplicationUserRole {
+import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum ApplicationUserRole {
+    DOCTOR(Sets.newHashSet()),
+    USER(Sets.newHashSet());
+
+
+    private final Set<ApplicationUserPermission> permission;
+
+     ApplicationUserRole(Set<ApplicationUserPermission> permission){
+        this.permission=permission;
+    }
+
+    public Set<ApplicationUserPermission> getPermission(){
+         return permission;
+    }
+
+
+    public Set<SimpleGrantedAuthority> grantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermission()
+                .stream().map(permission -> new SimpleGrantedAuthority(permission.getPermission())).
+                collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+        return permissions;
+    }
 }
