88046944496b849d2ca1fc9d71dd9b055481a923
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-10-21 10:06:22 +0530

    JwtConfig add properties and getter setter

--- src/main/java/com/nettaravel/com/com/nextrravel/jwt/JwtConfig.java
+++ src/main/java/com/nettaravel/com/com/nextrravel/jwt/JwtConfig.java
@@ -3,8 +3,44 @@ package com.nettaravel.com.com.nextrravel.jwt;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 @ConfigurationProperties(prefix = "application.jwt")
 public class JwtConfig {
+
+    private  String secretKey;
+
+    private String tokenPrefix;
+
+    private String tokenExpirationAfterDays;
+
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getTokenPrefix() {
+        return tokenPrefix;
+    }
+
+    public void setTokenPrefix(String tokenPrefix) {
+        this.tokenPrefix = tokenPrefix;
+    }
+
+    public String getTokenExpirationAfterDays() {
+        return tokenExpirationAfterDays;
+    }
+
+    public void setTokenExpirationAfterDays(String tokenExpirationAfterDays) {
+        this.tokenExpirationAfterDays = tokenExpirationAfterDays;
+    }
+
+    public String getAuthorizationHeader(){
+        return HttpHeaders.AUTHORIZATION;
+    }
 }
